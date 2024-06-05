dac658623d5d1b19d99560e6af4172fd4d8745af
sei-remote-share <125910218+sei-remote-share@users.noreply.github.com>
2024-06-05 16:04:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,18 @@ from comments_blueprint import comments_blueprint
 
 load_dotenv()
 
-def get_db_connection():
-    if 'ON_HEROKU' in os.environ:
-        connection = psycopg2.connect(
-            os.getenv('DATABASE_URL'), 
-            sslmode='require'
-        )
-    else:
-        connection = psycopg2.connect(
-            host='localhost',
-            database=os.getenv('POSTGRES_DATABASE'),
-            user=os.getenv('POSTGRES_USERNAME'),
-            password=os.getenv('POSTGRES_PASSWORD')
-        )
-    return connection
+if 'ON_HEROKU' in os.environ:
+    connection = psycopg2.connect(
+        os.getenv('DATABASE_URL'), 
+        sslmode='require'
+    )
+else:
+    connection = psycopg2.connect(
+        host='localhost',
+        database=os.getenv('POSTGRES_DATABASE'),
+        user=os.getenv('POSTGRES_USERNAME'),
+        password=os.getenv('POSTGRES_PASSWORD')
+    )
 
 
 app = Flask(__name__)
