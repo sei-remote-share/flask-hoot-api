03f508525a984c9b1628fadb3d836902d8efcd60
sei-remote-share <125910218+sei-remote-share@users.noreply.github.com>
2024-06-05 13:34:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,15 @@ from comments_blueprint import comments_blueprint
 
 load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(hoots_blueprint)
-    app.register_blueprint(authentication_blueprint)
-    app.register_blueprint(comments_blueprint)
+app = Flask(__name__)
+app.register_blueprint(hoots_blueprint)
+app.register_blueprint(authentication_blueprint)
+app.register_blueprint(comments_blueprint)
 
-    @app.route('/', methods=['GET'])
-    def signup():
-        return "Hello world."
-    
-    print(__name__)
+@app.route('/', methods=['GET'])
+def signup():
+    return "Hello world."
 
-    if __name__ == '__main__':
-        app.run()
-    else:
-        return app
-
-create_app()
+if __name__ == '__main__':
+    print('app is running')
+    app.run()
