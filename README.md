876e671a1d6eed6b43fb4b0b3dd7cdc619051d5f
sei-remote-share <125910218+sei-remote-share@users.noreply.github.com>
2024-06-05 12:02:28 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# ![Flask API - Hoot Back-End - Solution](./internal-resources/assets/hero.png)
+
+## About
+
+This repo contains solution code for the [Flask API - Hoot Back-End](https://git.generalassemb.ly/modular-curriculum-all-courses/flask-api-hoot-back-end.git) lecture.
+
+## Internal resources
+
+✏️ [Instructor Guide](./internal-resources/instructor-guide.md)
+
+🏗️ [Release Notes](./internal-resources/release-notes.md)
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from dotenv import load_dotenv
+from auth_blueprint import authentication_blueprint
+from hoots_blueprint import hoots_blueprint
+from comments_blueprint import comments_blueprint
+
+load_dotenv()
+
+app = Flask(__name__)
+app.register_blueprint(hoots_blueprint)
+app.register_blueprint(authentication_blueprint)
+app.register_blueprint(comments_blueprint)
+
+@app.route('/', methods=['GET'])
+def signup():
+  return "Hello world."
+
+app.run()
